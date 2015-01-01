c339689c8331255e1b8040785619817d74e257d1
NipunDesai <nipun@promactinfo.com>
2015-01-01 11:57:45 +0530

    initial commit

---
+++ DemoLearningApplication/App_Start/IocConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using DemoLearningApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac.Integration.Mvc;
+
+namespace DemoLearningApplication.App_Start
+{
+    public class IocConfig
+    {
+        public static IContainer Register()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterControllers(typeof(HomeController).Assembly);
+          
+
+            var container = containerBuilder.Build();
+
+            return container;
+        }
+    }
+}
---
+++ DemoLearningApplication/Models/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace DemoLearningApplication.Models.Context
+{
+    public class User : IdentityUser
+    {
+
+    }
+
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public DbSet<UserDetails> userDetails { get; set; }
+    }
+}
